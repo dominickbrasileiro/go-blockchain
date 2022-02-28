46984edb9679b9dfe90a8fc7f38048cca62982fa
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2022-02-28 16:06:13 -0300

    refactor: rename Block.Pow to Block.POW

--- block.go
+++ block.go
@@ -13,13 +13,13 @@ type Block struct {
 	Hash         string
 	PreviousHash string
 	Timestamp    time.Time
-	Pow          int
+	POW          int
 }
 
 func (b Block) CalculateHash() string {
 	data, _ := json.Marshal(b.Data)
 
-	dataToHash := b.PreviousHash + string(data) + b.Timestamp.String() + strconv.Itoa(b.Pow)
+	dataToHash := b.PreviousHash + string(data) + b.Timestamp.String() + strconv.Itoa(b.POW)
 
 	hash := sha256.Sum256([]byte(dataToHash))
 
