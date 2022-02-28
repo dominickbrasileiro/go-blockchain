c3ecab891fec2d9b365af4724bf985685cff197c
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2022-02-28 16:11:20 -0300

    feat: add CreateBlockchain function

--- blockchain.go
+++ blockchain.go
@@ -1,7 +1,22 @@
 package main
 
+import "time"
+
 type Blockchain struct {
 	GenesisBlock Block
 	Chain        []Block
 	Difficulty   int
+}
+
+func CreateBlockchain(difficulty int) Blockchain {
+	genesisBlock := Block{
+		Hash:      "0",
+		Timestamp: time.Now(),
+	}
+
+	return Blockchain{
+		GenesisBlock: genesisBlock,
+		Chain:        []Block{genesisBlock},
+		Difficulty:   difficulty,
+	}
 }
