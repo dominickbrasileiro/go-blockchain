dc68153f8f4dcb591fb7ee93df1daf86f79e5a2a
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2022-02-28 16:14:51 -0300

    feat: add AddBlock function to Blockchain

--- blockchain.go
+++ blockchain.go
@@ -8,6 +8,20 @@ type Blockchain struct {
 	Difficulty   int
 }
 
+func (b *Blockchain) AddBlock(data BlockData) {
+	lastBlock := b.Chain[len(b.Chain)-1]
+
+	newBlock := Block{
+		Data:         data,
+		PreviousHash: lastBlock.Hash,
+		Timestamp:    time.Now(),
+	}
+
+	newBlock.Mine(b.Difficulty)
+
+	b.Chain = append(b.Chain, newBlock)
+}
+
 func CreateBlockchain(difficulty int) Blockchain {
 	genesisBlock := Block{
 		Hash:      "0",
